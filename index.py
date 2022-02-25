ab5277e191eb9ab69fba42038963a87f8d65026e
erdalsatilmis <6634403@gmail.com>
2022-02-25 15:49:23 +0300

    Update index.py

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ def convertPDF():
 
 # start flask app
 if __name__ == "__main__":
- app.run(host="0.0.0.0", port=8080)
+ app.run(host="18.192.198.60", port=8080)
 #if __name__ == "__main__":
 #    app.run(ssl_context='adhoc')
 
